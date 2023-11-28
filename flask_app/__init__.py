67246e37d92e2432ccd5363ed4cc05c1bb2cad03
colby <dumonchelle123@gmail.com>
2023-11-27 19:09:27 -0500

    revert changes

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask, session
 app = Flask(__name__)
 UPLOAD_FOLDER = 'flask_app/static/images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = "This is my secret"
-
-app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.secret_key = "This is my secret"
