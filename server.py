fadabb60eef7d8702ef4a5737ab3dd3dd2b6e885
colby <dumonchelle123@gmail.com>
2023-11-27 19:01:38 -0500

    anoyther test

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from flask_app import app
 from flask_app.controllers import images
 
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 if __name__ == "__main__":
     app.run(debug=True)
