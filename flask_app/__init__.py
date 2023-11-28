de04a2c4d4cd3964eacb0941cf0c83889dccaf05
colby <dumonchelle123@gmail.com>
2023-11-27 19:00:45 -0500

    trying to fix html not updating

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -3,3 +3,6 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'flask_app/static/images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = "This is my secret"
+
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
